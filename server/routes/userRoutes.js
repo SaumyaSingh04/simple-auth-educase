c85744205c6de0d69bc86b93f1128e7e27a5ef2e
SaumyaSingh04 <saumya0419@gmail.com>
2025-05-24 15:01:28 +0530

    Fix import path and module type

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import cookieParser from 'cookie-parser';
 
