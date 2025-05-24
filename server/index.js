fad2a2547dfdc7d93af171a4c8e6db7c49c53d14
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-05-24 15:12:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin:      'http://localhost:5174',  
+  origin:      'https://simple-auth-educase.vercel.app',  
   credentials: true,
 }));
 
